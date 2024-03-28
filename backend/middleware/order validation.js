cedb37f1af1965c7a1100ec65d338f2517eca3fb
aman7ph <aman7ph@gmail.com>
2024-03-28 19:57:46 +0300

    resolve-conflict

--- backend/middleware/order validation.js
+++ backend/middleware/order validation.js
@@ -38,6 +38,8 @@ export const productValidation = (req, res, next) => {
   next();
 };
 
+import Joi from "joi";
+
 export const workerValidation = (req, res, next) => {
   const schema = Joi.object({
     name: Joi.string().required().min(3).max(20),
@@ -45,11 +47,7 @@ export const workerValidation = (req, res, next) => {
     salary: Joi.number().required().min(1),
     address: Joi.string().required(),
     phone_number: Joi.string().required().min(10).max(10),
-<<<<<<< HEAD
   }).options({ stripUnknown: true });
-=======
-  }).options({ stripUnknown: true })
->>>>>>> e2ae56fcb1b5a540563c90de0d51429f113155dc
 
   const { error } = schema.validate(req.body, { abortEarly: false });
   if (error) {
